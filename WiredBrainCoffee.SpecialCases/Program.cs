e2db17ae7752865451d91acf57fd385b6e385d8c
Farahalh <120371923+Farahalh@users.noreply.github.com>
2023-10-09 09:33:10 +0200

    Use static members in generic classes

--- WiredBrainCoffee.SpecialCases/Program.cs
+++ WiredBrainCoffee.SpecialCases/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            _ = new Container<string>();
+            _ = new Container<string>();
+            _ = new Container<int>();
+
+            Console.WriteLine($"Container<string>: {Container<string>.InstanceCount}");
+            Console.WriteLine($"Container<int>: {Container<int>.InstanceCount}");
+            Console.WriteLine($"Container<bool>: {Container<bool>.InstanceCount}");
+            Console.WriteLine($"ContainerBase: {ContainerBase.InstanceCountBase}");
+
+            Console.ReadLine();
         }
+    }
+
+    class ContainerBase
+    {
+        public ContainerBase() => InstanceCountBase++;
+
+        public static int InstanceCountBase { get; private set; }
+    }
+
+    class Container<T> : ContainerBase
+    {
+        public Container() => InstanceCount++;
+
+        public static int InstanceCount { get; private set; }
     }
 }
